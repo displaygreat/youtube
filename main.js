673d0c8bcfe9a151e275837d92f551a5f3d9618f
Oxana Shvartsman <displaygreat@gmail.com>
2021-04-08 23:56:23 +0300

    fix: menu and header; add: content

---
+++ main.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  slidesPerView: 6,
+  navigation: {
+    nextEl: '.channel-button-next',
+    prevEl: '.channel-button-prev',
+  },
+});
