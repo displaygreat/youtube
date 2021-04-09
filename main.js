b11ae91103a3c4fda4d2fb78f3f0ef0a3a353fcc
Oxana Shvartsman <displaygreat@gmail.com>
2021-04-09 19:06:34 +0300

    responsive

--- main.js
+++ main.js
@@ -1,8 +1,81 @@
-const swiper = new Swiper('.swiper-container', {
+const myChannelSlider = new Swiper('.channel-slider', {
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+    1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    },
+  },
   navigation: {
     nextEl: '.channel-button-next',
     prevEl: '.channel-button-prev',
   },
 });
+const recommendedSlider = new Swiper('.recommendedSlider', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1600: {
+      slidesPerView: 3
+    },
+    1100: {
+      slidesPerView: 2
+    },
+  },
+  navigation: {
+    nextEl: '.recommended-button-next',
+    prevEl: '.recommended-button-prev',
+  },
+});
+const recommendedChannelSlider = new Swiper('.recommended-channel-slider', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+    1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    },
+  },
+  navigation: {
+    nextEl: '.recommended-channel-button-next',
+    prevEl: '.recommended-channel-button-prev',
+  },
+});
+
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
+});
+
+if (document.documentElement.scrollWidth <= 640) {
+  myChannelSlider.destroy();
+  // recommendedSlider.destroy();
+  // recommendedChannelSlider.destroy();
+}
